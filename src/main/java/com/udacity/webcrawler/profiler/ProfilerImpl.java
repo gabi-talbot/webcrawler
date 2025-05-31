8740e6fbde2cc4acf2e00a10092735e2d62e27d9
Gabi <talbotgabi@gmail.com>
2025-05-31 10:06:03 +0100

    All tests passing

--- src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -3,9 +3,15 @@ package com.udacity.webcrawler.profiler;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
@@ -28,18 +34,37 @@ final class ProfilerImpl implements Profiler {
   @Override
   public <T> T wrap(Class<T> klass, T delegate) {
     Objects.requireNonNull(klass);
-
     // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
+    List<Method> methodList = new ArrayList<>(Arrays.asList(klass.getDeclaredMethods()));
 
-    return delegate;
+    if (methodList.isEmpty()) {
+      throw new IllegalArgumentException("No suitable profiler methods found");
+    }
+
+    ProfilingMethodInterceptor interceptor = new ProfilingMethodInterceptor(clock, delegate, state, startTime);
+
+    Object proxy = Proxy.newProxyInstance(
+            ProfilerImpl.class.getClassLoader(),
+            new Class[]{klass},
+            interceptor);
+
+    return (T) proxy;
+
   }
 
   @Override
   public void writeData(Path path) {
     // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
     //       path, the new data should be appended to the existing file.
+
+    try(Writer writer = Files.newBufferedWriter(path)) {
+      writeData(writer);
+      writer.flush();
+    } catch (IOException e) {
+      e.getMessage();
+    }
   }
 
   @Override
